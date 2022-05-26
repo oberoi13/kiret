2549118682179373b63b94387577b65be7ccc982
oberoi13 <105886263+oberoi13@users.noreply.github.com>
2022-05-26 14:26:26 -0400

    Update JavaApplication1.java

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
     System.out.println("heloo");
     System.out.println("hello world");
+     System.out.println("hello ld");
         // TODO code application logic here
     }
     
